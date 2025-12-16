81e721b0acfb58ecfa1f5d8835652528a9b238d4
nadhivv <dhivnadhiv@gmail.com>
2025-12-16 15:33:18 +0700

    fix route achievement

--- route/achievement_route.go
+++ route/achievement_route.go
@@ -1,49 +1,47 @@
 package route
 
 import (
-	"UAS/app/repository"
-	"UAS/app/service"
-	"UAS/middleware"
-	"UAS/database"
+    "UAS/app/repository"
+    "UAS/app/service"
+    "UAS/middleware"
+    "UAS/database"
 
-	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/mongo"
+    "github.com/gofiber/fiber/v2"
+    "go.mongodb.org/mongo-driver/mongo"
 )
 
 func SetupAchievementRoutes(router fiber.Router, 
-	userRepo repository.UserRepository,
-	roleRepo repository.RoleRepository,
-	studentRepo repository.StudentRepository,
-	lecturerRepo repository.LecturerRepository,
-	mongoDB *mongo.Database) {
+    userRepo repository.UserRepository,
+    roleRepo repository.RoleRepository,
+    studentRepo repository.StudentRepository,
+    lecturerRepo repository.LecturerRepository,
+    mongoDB *mongo.Database) {
 
-	// Inisialisasi repositories
-	achievementRefRepo := repository.NewAchievementReferenceRepository(database.PgDB)
-	achievementRepo := repository.NewAchievementRepository(mongoDB.Collection("achievements"))
-	
-	// Inisialisasi service
-	achievementService := service.NewAchievementService(
-		achievementRepo,
-		achievementRefRepo,
-		studentRepo,
-		lecturerRepo,
-		userRepo,
-		roleRepo,
-	)
+    // Inisialisasi repositories
+    achievementRefRepo := repository.NewAchievementReferenceRepository(database.PgDB)
+    achievementRepo := repository.NewAchievementRepository(mongoDB.Collection("achievements"))
+    
+    achievementService := service.NewAchievementService(
+        achievementRepo,
+        achievementRefRepo,
+        studentRepo,
+        lecturerRepo,
+        userRepo,
+        roleRepo,
+    )
 
-	achievementRoutes := router.Group("/achievements")
-	achievementRoutes.Use(middleware.RequireAuth(userRepo))
+    achievementRoutes := router.Group("/achievements")
+    achievementRoutes.Use(middleware.RequireAuth(userRepo))
 
-	achievementRoutes.Get("/", achievementService.GetAllAchievements)
-	achievementRoutes.Get("/:id", achievementService.GetAchievementByID)
-	achievementRoutes.Post("/", achievementService.CreateAchievement)
-	achievementRoutes.Put("/:id", achievementService.UpdateAchievement)
-	achievementRoutes.Delete("/:id", achievementService.DeleteAchievement)
-	achievementRoutes.Post("/:id/submit", achievementService.SubmitAchievement)
-	achievementRoutes.Post("/:id/verify", achievementService.VerifyAchievement)
-	achievementRoutes.Post("/:id/reject", achievementService.RejectAchievement)
-	
-	// achievementRoutes.Get("/:id/history", achievementService.GetAchievementHistory)
-	
-	// achievementRoutes.Post("/:id/attachments", achievementService.UploadAttachment)
+    achievementRoutes.Get("/", achievementService.GetAllAchievements)
+    achievementRoutes.Get("/:id", achievementService.GetAchievementByID)
+    achievementRoutes.Post("/", achievementService.CreateAchievement)
+    achievementRoutes.Put("/:id", achievementService.UpdateAchievement)
+    achievementRoutes.Delete("/:id", achievementService.DeleteAchievement)
+    achievementRoutes.Post("/:id/submit", achievementService.SubmitAchievement)
+    achievementRoutes.Post("/:id/verify", achievementService.VerifyAchievement)
+    achievementRoutes.Post("/:id/reject", achievementService.RejectAchievement)
+    achievementRoutes.Get("/:id/history", achievementService.GetAchievementHistory)
+    achievementRoutes.Post("/:id/attachments", achievementService.UploadAttachment)
+
 }
